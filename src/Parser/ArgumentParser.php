0db13b5c4ff09ca8495b3ea2a3b2563c63bce917
Maxim <defilak@mail.ru>
2022-08-29 15:54:51 +0300

    добавил описание, навел марафет

--- src/Parser/ArgumentParser.php
+++ src/Parser/ArgumentParser.php
@@ -4,6 +4,10 @@ namespace Console\Parser;
 
 use Exception;
 
+/**
+ * Класс для парсинга и доступа к параметрам и аргументом в установленном формате.
+ * todo: сделать без str_starts_with.
+ */
 class ArgumentParser
 {
     /**
@@ -18,34 +22,28 @@ class ArgumentParser
     const OPTIONS_REGEX = '/\[([^\]]\w+)=(.+)\]/';
 
     private array $arguments = [];
+    
     private array $options = [];
 
-    public function __construct($commandLine)
+    public function __construct(array $commandLine)
     {
-        $arguments = [];
-        $options = [];
-        foreach ($commandLine as $part) {
-            if (str_starts_with($part, '{')) {
-                $arg = $this->parseArg($part)[0];
-                $arguments = array_merge($arguments, $arg);
-
-            } else if (str_starts_with($part, '[')) {
-                $option = $this->parseOption($part);
-                $options = array_merge($options, $option);
-
+        foreach ($commandLine as $str) {
+            if (str_starts_with($str, '{')) {
+                $arg = $this->parseArg($str);
+                $this->arguments = array_merge($this->arguments, $arg);
+            } else if (str_starts_with($str, '[')) {
+                $opt = $this->parseOption($str);
+                $this->options = array_merge($this->options, $opt);
             } else {
-                throw new Exception("Неправильный формат аргументов! $part");
+                throw new Exception("Неправильный формат аргументов! $str");
             }
         }
-
-        $this->arguments = $arguments;
-        $this->options = $options;
     }
 
     private function parseArg($str)
     {
         preg_match_all(static::ARGS_REGEX, $str, $matches);
-        return $matches;
+        return $matches[0];
     }
 
     private function parseOption($str)
@@ -53,37 +51,43 @@ class ArgumentParser
         preg_match(static::OPTIONS_REGEX, $str, $matches);
         $key = $matches[1];
         $value = $matches[2];
-        if(str_starts_with($value, '{')) {
+        if (str_starts_with($value, '{')) {
             $value = $this->parseArg($value);
         }
 
         return [$key => $value];
     }
 
-    public function getArgument($name)
+    /**
+     * Возвращает true если данный аргумент был передан.
+     */
+    public function getArgument($name): bool
     {
-        return $this->arguments[$name] ?? false;
-    }
-
-    public function getOption($name)
-    {
-        return $this->arguments[$name] ?? false;
+        return in_array($name, $this->arguments);
     }
 
     /**
-     * Get the value of arguments
+     * Возвращает данные опции если она была передана. Иначе false.
      */
-    public function getArguments()
+    public function getOption($name): mixed
     {
-        return $this->arguments;
+        return $this->options[$name] ?? false;
     }
 
     /**
-     * Get the value of options
+     * Возвращает копию массива со всеми переданными аргументами.
      */
-    public function getOptions()
+    public function getArguments(): array
     {
-        return $this->options;
+        return clone $this->arguments;
+    }
+
+    /**
+     * Возвращает копию массива со всеми переданными опцями и их параметры.
+     */
+    public function getOptions(): array
+    {
+        return clone $this->options;
     }
 
     public function __toString()
